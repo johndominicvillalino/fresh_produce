05b65a68d13301e335a98e2a7221a8f2f6cbe708
JohnDv <johndominicvillalino@outlook.com>
2022-08-23 18:22:38 +0800

    devise user init

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+        protect_from_forgery with: :null_session
+        before_action :configure_permitted_parameters, if: :devise_controller?
+        include DeviseTokenAuth::Concerns::SetUserByToken
+
+        protected
+
+        def configure_permitted_parameters
+          devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :user_name, :last_name, :image])
+        end
 end
