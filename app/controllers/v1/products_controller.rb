fc1f0cee8662c68052878d5282260c1951a81897
grailwilly <grailjwi11y@gmail.com>
2022-08-24 21:54:00 +0800

    Add product create method

--- app/controllers/v1/products_controller.rb
+++ app/controllers/v1/products_controller.rb
@@ -1,7 +1,30 @@
 class V1::ProductsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
-  def index
-    @products = Products.All 
-    render :json => @products
+  def new
+    @product = Product.new
+    @product.user_id = current_user.id
+    render :json => @product
+  end
+
+  def create
+    @product = Product.create(product_params)
+    @product.user_id = current_user.id
+
+    respond_to do |format|
+      if @product.save
+        format.json  { render :json => @product,
+                      status: :created }
+      else
+        format.json  { render :json => @product.errors,
+                      :status => :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:image, :name, :price, :minimum_order, :qty_measurement, :units_available, :harvest_time, :product_type, :estimated_delivery, :user_id)
   end
 end
