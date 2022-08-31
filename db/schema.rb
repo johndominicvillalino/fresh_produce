08fe0b8d31e6f8bbc055c28afc9c331c7a105275
grailwilly <grailjwi11y@gmail.com>
2022-08-31 21:34:20 +0800

    Add columns bio and address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_27_085115) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_31_113126) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_27_085115) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "product_id"
+    t.bigint "product_id", null: false
     t.index ["product_id"], name: "index_carts_on_product_id"
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
@@ -47,8 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_27_085115) do
     t.string "order_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -88,6 +86,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_27_085115) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "role"
+    t.string "bio"
+    t.string "city"
+    t.string "barangay"
+    t.string "town"
+    t.string "province"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
@@ -98,6 +101,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_27_085115) do
   add_foreign_key "carts", "users"
   add_foreign_key "ordered_items", "orders"
   add_foreign_key "ordered_items", "products"
-  add_foreign_key "orders", "users"
   add_foreign_key "products", "users"
 end
